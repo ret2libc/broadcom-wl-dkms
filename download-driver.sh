ba252aa7945058b0e8d7b9c7af2566de615471b5
Riccardo Schirone <sirmy15@gmail.com>
2017-11-24 00:40:26 +0100

    Initial commit

---
+++ download-driver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://docs.broadcom.com/docs-and-downloads/docs/linux_sta/hybrid-v35_64-nodebug-pcoem-6_30_223_271.tar.gz
+tar xvf hybrid-v35_64-nodebug-pcoem-6_30_223_271.tar.gz
